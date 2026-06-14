93fd71930e705eabf5cc8426dcda8406d03b0887
SaadDev <saad26dev@gmail.com>
2026-06-14 18:13:55 +0530

    PayrollSystem code completed

--- src/PayrollSystem.java
+++ src/PayrollSystem.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+
 public class PayrollSystem {
+    private ArrayList<Employee> employeesList;
+
+    public PayrollSystem(){
+        employeesList = new ArrayList<>();
+    }
+
+    public void addEmployee(Employee employee) {
+        employeesList.add(employee);
+    }
+
+    public void removeEmployee(int id) {
+       Employee employeeToRemove = null;
+       for(Employee employee:employeesList){
+           if (employee.getId() == id) {
+               employeeToRemove = employee;
+               break;
+           }
+       }
+        if (employeeToRemove != null) {
+            employeesList.remove(employeeToRemove);
+        }
+    }
+
+    public void displayEmployees(){
+        for (Employees emplyee : employeesList) {
+            System.out.println(emplyee);
+        }
+
+    }
 }

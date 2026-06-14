df4413b581ce972d6baa1d56f4550f59fbbd5b0e
SaadDev <saad26dev@gmail.com>
2026-06-14 17:15:23 +0530

    Completed FullTimeEmployee class

--- src/FullTimeEmployee.java
+++ src/FullTimeEmployee.java
@@ -1,2 +1,13 @@
 public class FullTimeEmployee extends Employee{
+   private double monthlySalary;
+
+    public FullTimeEmployee(String name, int id, double monthlySalary){
+        // this calls the constructor of super class
+        super(name, id);
+        this.monthlySalary = monthlySalary;
+    }
+    @Override
+    public double calculateSalary(){
+        return monthlySalary;
+    }
 }

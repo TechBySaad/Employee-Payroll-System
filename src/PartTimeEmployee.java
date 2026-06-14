86d380cd15a94e07d7cdafac68cb470d57f4ca86
SaadDev <saad26dev@gmail.com>
2026-06-14 19:43:13 +0530

    added part time employee class

--- src/PartTimeEmployee.java
+++ src/PartTimeEmployee.java
@@ -1,16 +1,19 @@
-public class PartTimeEmployee extends Employee{
+
+public class PartTimeEmployee extends Employee {
     private int hoursWorked;
     private double hourlyRate;
 
-    public PartTimeEmployee(String name, int id, String job, int hoursWorked, double hourlyRate){
+    public PartTimeEmployee(String name, int id, String job, int hoursWorked, double hourlyRate) {
         super(name, id, job);
         this.hoursWorked = hoursWorked;
         this.hourlyRate = hourlyRate;
     }
 
+    public double getHourlyRate() { return hourlyRate; }
+    public int getHoursWorked() { return hoursWorked; }
+
     @Override
-    public double calculateSalary(){
-        return hoursWorked*hourlyRate;
+    public double calculateSalary() {
+        return hoursWorked * hourlyRate;
     }
 }
-
